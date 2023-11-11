415896122825fb07526cca366cf22fc146f266bb
JRParreno <jhonrhayparreno22@gmail.com>
2023-11-11 11:41:03 +0800

    test static files storage

--- fire_alert_core/settings.py
+++ fire_alert_core/settings.py
@@ -207,7 +207,7 @@ if not DEBUG:
 
     # Turn on WhiteNoise storage backend that takes care of compressing static files
     # and creating unique names for each version so they can safely be cached forever.
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
